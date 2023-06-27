4d55aa00d9f7ead407f604b0cbf9c7c9832357d9
Lightning <2905561708@qq.com>
2023-06-27 20:08:57 +0800

    123

--- STUCMS/settings.py
+++ STUCMS/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         # 连接mysql数据库的用户名
         'USER': 'root',
         # 连接mysql数据库的密码
-        'PASSWORD': '123456',
+        'PASSWORD': 'mysql123',
         # mysql数据库的主机地址
         'HOST': '127.0.0.1',
         # mysql数据库的端口号
